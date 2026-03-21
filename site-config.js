4f32e41788c149ea83cdc97b3b96bd8b3099bc61
PIXORA24 <pixora24@gmail.com>
2026-03-22 00:21:37 +0530

    Add files via upload

---
+++ site-config.js
@@ -0,0 +1,97 @@
+// Generated from site-data.json via build-site.ps1
+window.SITE_CONFIG = {
+    "site":  {
+                 "baseUrl":  "https://pixora24.github.io/Ad_invites_2026/",
+                 "pageTitle":  "Raksha \u0026 Dhiraj Invitation",
+                 "landing":  {
+                                 "title":  "Raksha \u0026 Dhiraj Celebration",
+                                 "description":  "With love and joy, we invite you to celebrate our special moments. Tap to view the digital invitation with event details.",
+                                 "twitterTitle":  "Raksha \u0026 Dhiraj Celebration",
+                                 "twitterDescription":  "With love and joy, we invite you to celebrate our special moments. Tap to view the digital invitation with event details.",
+                                 "image":  "assets/previews/site-preview.webp"
+                             }
+             },
+    "studio":  {
+                   "enabled":  true,
+                   "name":  "Pixora24",
+                   "label":  "Crafted by",
+                   "url":  "https://www.instagram.com/pixora24?igsh=eWRldmc2emluNzU="
+               },
+    "events":  [
+                   {
+                       "key":  "wedding",
+                       "title":  "Wedding",
+                       "pageTitle":  "Raksha \u0026 Dhiraj Wedding",
+                       "cardLabel":  "Wedding",
+                       "cardAlt":  "Wedding Invite",
+                       "cardImage":  "assets/wedding/thumb.webp",
+                       "labelClass":  "wedding-label",
+                       "landingPage":  "wedding.html",
+                       "sharePages":  [
+                                          {
+                                              "file":  "wedding_solo.html",
+                                              "title":  "Raksha \u0026 Dhiraj Wedding Invite",
+                                              "description":  "Hey!With lots of love, we invite you to our wedding 💍We’d be really happy to have you with us on our special day.Please find our invitation attached.Raksha \u0026 Dhiraj",
+                                              "image":  "assets/previews/wedding-solo-preview.webp"
+                                          }
+                                      ],
+                       "share":  {
+                                     "title":  "Raksha \u0026 Dhiraj Wedding Invitation",
+                                     "description":  "2 June 2026 | 11:00 AM | Vanitha Achuth Pai Convention Centre, Temple, MarryHill, Behind Padavinangadi, Airport Rd, Mary Hill, Konchady, Mangaluru, Karnataka 575008",
+                                     "image":  "assets/previews/wedding-preview.webp"
+                                 },
+                       "media":  {
+                                     "video":  "assets/wedding/video.mp4",
+                                     "audio":  "assets/wedding/music.mp3",
+                                     "poster":  "assets/wedding/bg.webp"
+                                 },
+                       "calendarTitle":  "Raksha \u0026 Dhiraj Wedding",
+                       "calendarDurationHours":  2,
+                       "startDate":  "2026-06-02T11:00:00+05:30",
+                       "venue":  "Vanitha Achuth Pai Convention Centre, Temple, MarryHill, Behind Padavinangadi, Airport Rd, Mary Hill, Konchady, Mangaluru, Karnataka 575008",
+                       "mapQuery":  "Vanitha Achuth Pai Convention Centre, Temple, MarryHill, Behind Padavinangadi, Airport Rd, Mary Hill, Konchady, Mangaluru, Karnataka 575008",
+                       "directionsEnabled":  true,
+                       "calendarEnabled":  true,
+                       "countdownEnabled":  true,
+                       "location":  {
+                                        "lat":  "12.908924348851945",
+                                        "lng":  "74.86648017969364"
+                                    }
+                   },
+                   {
+                       "key":  "reception",
+                       "title":  "Reception",
+                       "pageTitle":  "Raksha \u0026 Dhiraj Reception",
+                       "cardLabel":  "Reception",
+                       "cardAlt":  "Reception Invite",
+                       "cardImage":  "assets/reception/thumb.webp",
+                       "labelClass":  "",
+                       "landingPage":  "reception.html",
+                       "sharePages":  [
+
+                                      ],
+                       "share":  {
+                                     "title":  "Raksha \u0026 Dhiraj Reception Invitation",
+                                     "description":  "3 June 2026 | 7:00 PM | TMA Pai Convention Centre, MG Rd, Kodailbail, Mangaluru, Karnataka 575003",
+                                     "image":  "assets/previews/reception-preview.webp"
+                                 },
+                       "media":  {
+                                     "video":  "assets/reception/video.mp4",
+                                     "audio":  "assets/reception/music.mp3",
+                                     "poster":  "assets/reception/bg.webp"
+                                 },
+                       "calendarTitle":  "Raksha \u0026 Dhiraj Reception",
+                       "calendarDurationHours":  3,
+                       "startDate":  "2026-06-03T19:00:00+05:30",
+                       "venue":  "TMA Pai Convention Centre, MG Rd, Kodailbail, Mangaluru, Karnataka 575003",
+                       "mapQuery":  "TMA Pai Convention Centre, MG Rd, Kodailbail, Mangaluru, Karnataka 575003",
+                       "directionsEnabled":  true,
+                       "calendarEnabled":  true,
+                       "countdownEnabled":  true,
+                       "location":  {
+                                        "lat":  "12.880434781782897",
+                                        "lng":  "74.83978505085807"
+                                    }
+                   }
+               ]
+};
